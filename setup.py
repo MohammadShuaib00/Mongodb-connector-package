2ea37607145ccd12097aa5654d67b567066004e4
Mohammad Shuaib <mohammadshuaib3455@gmail.com>
2024-08-06 12:59:49 +0530

    Code and Version Successfully Updated

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()
 
-__version__ = "0.0.1"
+__version__ = "0.0.3"
 REPO_NAME = "mongodb-connector-package"
 PKG_NAME = "dbautomationtool"
 AUTHOR_USER_NAME = "Mohammad Shuaib"
